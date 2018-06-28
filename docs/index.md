789804a9a699e899f5f748f4c0f85effb5429a97
Iain Reid <iainreid820@users.noreply.github.com>
2018-06-28 11:57:02 +0100

    Add initial documentation (#1)
    * Add initial documentation
    
    * Correct "cacheing" to "caching"
    
    * Correct typos

---
+++ docs/index.md
@@ -0,0 +1,17 @@
+# Home
+
+Welcome to the documentation site for WCM, the dependency manager for Web Components.
+
+!!! warning
+    This project is currenty in beta, and while we're confident that it's awesome it's certainly not production ready!
+
+## Getting started
+
+If you haven't done so already, install the command-line tool. This package includes every you'll need to incorporate all the benefits of
+WCM into your project.
+
+```bash
+npm i wcm -g
+```
+
+!!! note "How awkward, we're still working on this!"
--- src/main/util/classes/Config.ts
+++ src/main/util/classes/Config.ts
@@ -1,10 +1,25 @@
+/**
+ * A simple configuration store with typesafe `get` and `set` methods. An array of `assignableKeys` is assigned on
+ * construction to provide a safe iterable when listing the configuration keys.
+ */
 export class Config<T> {
   constructor(private config: T, public assignableKeys: Array<keyof T> = Object.keys(config) as (keyof T)[]) {}
 
+  /**
+   * Retrieve the value assigned to the key provided.
+   *
+   * @param key
+   */
   public get<K extends keyof T>(key: K): T[K] {
     return this.config[key];
   }
 
+  /**
+   * Assign the value to the key provided.
+   *
+   * @param key
+   * @param value
+   */
   public set<K extends keyof T>(key: K, value: T[K]): void {
     this.config[key] = value;
   }
