b1ae933d0f34da9c0cf1e0893e108241fe06bcb8
Iain Reid <iainreid382@gmail.com>
2019-09-10 10:01:40 +0000

    Fix failing build

--- src/main/lib/config/index.ts
+++ src/main/lib/config/index.ts
@@ -5,15 +5,13 @@ import { formatAlert, LogType } from "../../util/methods/logging";
 import { BrowserConfig } from "./../browser/config";
 import { BundleConfig } from "../bundle/config";
 import { MigrationConfig } from "./../migration/config";
-import { ProxyConfig } from "./../proxy/config";
 
 require("cosmiconfig").default = require("cosmiconfig");
 
 export const GlobalConfig = {
   browser: ConfigFactory(BrowserConfig),
   bundle: ConfigFactory(BundleConfig),
-  migration: ConfigFactory(MigrationConfig),
-  proxy: ConfigFactory(ProxyConfig)
+  migration: ConfigFactory(MigrationConfig)
 };
 
 export default async function(vorpal: Vorpal) {
@@ -34,9 +32,6 @@ export default async function(vorpal: Vorpal) {
           break;
         case "migration":
           GlobalConfig.migration.updateInstance(result.config[namespace]);
-          break;
-        case "proxy":
-          GlobalConfig.proxy.updateInstance(result.config[namespace]);
           break;
         default:
           vorpal.log(formatAlert(vorpal, LogType.ERROR, 'Unknown configuration key "%s".', namespace));
--- src/main/wcm.ts
+++ src/main/wcm.ts
@@ -2,18 +2,11 @@ import * as vorpal from "vorpal";
 import browser from "./lib/browser";
 import bundle from "./lib/bundle";
 import config from "./lib/config";
-import install from "./lib/install";
 import migration from "./lib/migration";
-import pack from "./lib/pack";
-import proxy from "./lib/proxy";
 
 export const cli = (new vorpal)
   .history(".wcm_history")
   .use(browser)
   .use(bundle)
   .use(config)
-  .use(install)
-  .use(migration)
-  .use(pack)
-  .use(proxy);
-  
+  .use(migration);
